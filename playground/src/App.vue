8acf42599722d2eb57aacf8a80c5b160118f3d33
付志强 <zhiqiangfu6@gmail.com>
2024-04-25 22:59:46 +0800

    feat: playgound

---
+++ playground/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <canvas></canvas>
+</template>
+
+<script setup>
+import { createTextImage } from '@sunny-117/text-image';
+import {onMounted} from 'vue';
+
+onMounted(() => {
+  createTextImage({
+    canvas: document.querySelector('canvas'),
+    source: {
+      text: 'Text Image',
+    },
+  });
+})
+</script>
+
+<style scoped>
+.titleBox {
+  background-color: blueviolet;
+}
+</style>
